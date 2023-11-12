7800f8cbc6fc69e0256bb7c26fb7b8ba10ec594e
shararaviget9 <isharasiriwardhanaget99@gmail.com>
2023-11-12 16:55:15 +0530

    initial commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -55,7 +55,7 @@ export const Skills = () => {
               //solves IE problem
               this.toFront();
 
-            $(".skills")
+            $(".myskills")
               .find("ul li:nth-child(" + order + ")")
               .each(function () {
                 $(this).addClass("hovered");
@@ -67,7 +67,7 @@ export const Skills = () => {
               "elastic"
             );
 
-            $(".skills")
+            $(".myskills")
               .find("ul li:nth-child(" + order + ")")
               .each(function () {
                 $(this).removeClass("hovered");
@@ -81,52 +81,53 @@ export const Skills = () => {
   });
 
   return (
-    <section className="skill" id="skills">
+    <section className="skillsPage" id="skills">
       <div className="container">
         <div className="row">
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
               <h2>Skills</h2>
-
-              <div id="content">
-                <div className="legend">
-                  <h1>Legend:</h1>
-                  <div class="skills">
-                    <ul>
-                      <li className="myList">Fullstack Developer</li>
-                      <li className="myList">Computer Science</li>
-                      <li className="myList">Generative AI</li>
-                      <li className="myList">Artificial Intelligence (AI)</li>
-                    </ul>
+              <div className="newColom">
+                <div id="content">
+                  <div className="legend">
+                    <h1>Legend:</h1>
+                    <div class="myskills">
+                      <ul>
+                        <li className="myList">Fullstack Developer</li>
+                        <li className="myList">Computer Science</li>
+                        <li className="myList">Generative AI</li>
+                        <li className="myList">Artificial Intelligence (AI)</li>
+                      </ul>
+                    </div>
                   </div>
+                  <div id="diagram"></div>
                 </div>
-                <div id="diagram"></div>
-              </div>
 
-              <div className="get">
-                <div className="arc">
-                  {/* <!--  Fullstack Developer --> */}
-                  <input type="hidden" className="percent" value="99" />
-                  <input type="hidden" className="color" value="#BEDBE9" />
-                  <input type="hidden" className="order" value="1" />
-                </div>
-                <div class="arc">
-                  {/*  <!--  Computer Science --> */}
-                  <input type="hidden" class="percent" value="90" />
-                  <input type="hidden" class="color" value="#97BE0D" />
-                  <input type="hidden" class="order" value="2" />
-                </div>
-                <div class="arc">
-                  {/* <!-- Generative AI --> */}
-                  <input type="hidden" class="percent" value="90" />
-                  <input type="hidden" class="color" value="#D84F5F" />
-                  <input type="hidden" class="order" value="3" />
-                </div>
-                <div class="arc">
-                  {/*  <!--   Artificial Intelligence (AI) --> */}
-                  <input type="hidden" class="percent" value="90" />
-                  <input type="hidden" class="color" value="#88B8E6" />
-                  <input type="hidden" class="order" value="4" />
+                <div className="get">
+                  <div className="arc">
+                    {/* <!--  Fullstack Developer --> */}
+                    <input type="hidden" className="percent" value="99" />
+                    <input type="hidden" className="color" value="#BEDBE9" />
+                    <input type="hidden" className="order" value="1" />
+                  </div>
+                  <div class="arc">
+                    {/*  <!--  Computer Science --> */}
+                    <input type="hidden" class="percent" value="90" />
+                    <input type="hidden" class="color" value="#97BE0D" />
+                    <input type="hidden" class="order" value="2" />
+                  </div>
+                  <div class="arc">
+                    {/* <!-- Generative AI --> */}
+                    <input type="hidden" class="percent" value="90" />
+                    <input type="hidden" class="color" value="#D84F5F" />
+                    <input type="hidden" class="order" value="3" />
+                  </div>
+                  <div class="arc">
+                    {/*  <!--   Artificial Intelligence (AI) --> */}
+                    <input type="hidden" class="percent" value="90" />
+                    <input type="hidden" class="color" value="#88B8E6" />
+                    <input type="hidden" class="order" value="4" />
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/profile.js
+++ src/components/profile.js
@@ -5,72 +5,68 @@ export const Profile = () => {
   return (
     <section className="profile" id="profile">
       <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="skill-bx wow zoomIn">
-              <h2>About Me</h2>
-              <div className="newColom">
-                <div className="profile_me">
-                  <div className="image">
-                    <img src={profileme} alt="profile_me"></img>
-                  </div>
-                  <h3 className="myname">
-                    Ishara Siriwardhana <i class="ri-lightbulb-flash-fill"></i>
-                  </h3>
+        <div className="profile-bx wow zoomIn">
+          <h2>About Me</h2>
+          <div className="newColom">
+            <div className="profile_me">
+              <div className="image">
+                <img src={profileme} alt="profile_me"></img>
+              </div>
+              <h3 className="myname">
+                Ishara Siriwardhana <i class="ri-lightbulb-flash-fill"></i>
+              </h3>
 
-                  <p>
-                    <i class="ri-edit-2-fill">
-                      <span> 1000+</span>
-                    </i>
+              <p>
+                <i class="ri-edit-2-fill">
+                  <span> 1000+</span>
+                </i>
 
-                    <i class="ri-verified-badge-fill">
-                      <span> 9+</span>
-                    </i>
+                <i class="ri-verified-badge-fill">
+                  <span> 9+</span>
+                </i>
 
-                    <i class="ri-thumb-up-fill">
-                      <span> 20+</span>
-                    </i>
-                  </p>
+                <i class="ri-thumb-up-fill">
+                  <span> 20+</span>
+                </i>
+              </p>
+            </div>
+            <div className="discript">
+              <div className="icons">
+                <div className="icon_logo">
+                  <i class="ri-medal-line"></i>
+                  <sub>Reword</sub>
+                  <sup>8+</sup>
+                  <div className="count"></div>
                 </div>
-                <div className="discript">
-                  <div className="icons">
-                    <div className="icon_logo">
-                      <i class="ri-medal-line"></i>
-                      <sub>Reword</sub>
-                      <sup>8+</sup>
-                      <div className="count"></div>
-                    </div>
-                    <div className="icon_logo">
-                      <i class="ri-git-repository-fill"></i>
-                      <sub>Git Repository</sub>
-                      <sup>9+</sup>
-                      <div className="count"></div>
-                    </div>
-                    <div className="icon_logo">
-                      <i class="ri-timer-flash-line"></i>
-                      <sub>Flash Time</sub>
-                      <sup>-0.2s</sup>
-                      <div className="count"></div>
-                    </div>
-                    <div className="icon_logo">
-                      <i class="ri-terminal-box-fill"></i>
-                      <sub>Development</sub>
-                      <sup>20+</sup>
-                      <div className="count"></div>
-                    </div>
-                  </div>
-
-                  <p>
-                    Computer scientist skills are a combination of hard skills
-                    like technical knowledge and soft skills necessary to
-                    collaborate in teams and achieve objectives creatively.
-                    Coding, software development and technical writing are just
-                    a few of the hard skills for computer science. Communication
-                    and teamwork are two examples of important soft skills that
-                    computer scientists can implement in their work.
-                  </p>
+                <div className="icon_logo">
+                  <i class="ri-git-repository-fill"></i>
+                  <sub>Git Repository</sub>
+                  <sup>9+</sup>
+                  <div className="count"></div>
+                </div>
+                <div className="icon_logo">
+                  <i class="ri-timer-flash-line"></i>
+                  <sub>Flash Time</sub>
+                  <sup>-0.2s</sup>
+                  <div className="count"></div>
+                </div>
+                <div className="icon_logo">
+                  <i class="ri-terminal-box-fill"></i>
+                  <sub>Development</sub>
+                  <sup>20+</sup>
+                  <div className="count"></div>
                 </div>
               </div>
+
+              <p>
+                Computer scientist skills are a combination of hard skills like
+                technical knowledge and soft skills necessary to collaborate in
+                teams and achieve objectives creatively. Coding, software
+                development and technical writing are just a few of the hard
+                skills for computer science. Communication and teamwork are two
+                examples of important soft skills that computer scientists can
+                implement in their work.
+              </p>
             </div>
           </div>
         </div>
