2cba4dbd187a262ec32c5273397700f0da2b067b
shararaviget9 <isharasiriwardhanaget99@gmail.com>
2024-01-04 01:13:48 +0530

    first commiting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/pwithc_1.png";
+import projImg2 from "../assets/img/swoodwork.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
@@ -9,8 +10,16 @@ export const Projects = () => {
     {
       title: "PEACE with CONTENTMENT",
       description:
-        "develop a web application for Talk therapy, Mindfulness and meditation, Symptom tracking, Cognitive-behavioral therapy (CBT), Self-help resources",
+        "Develop a web application for Talk therapy, Mindfulness and meditation, Symptom tracking, Cognitive-behavioral therapy (CBT), Self-help resources",
       imgUrl: projImg1,
+      url: "https://example.com",
+    },
+    {
+      title: "SIRIWARDHANAN WOODWORK",
+      description:
+        "Develop a web application for Woodworking products marketplace and publish his skill and performance. ",
+      imgUrl: projImg2,
+      url: "https://ishararaviget9.github.io/Woodworking/",
     },
   ];
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,45 +1,113 @@
 import "../default.css";
+import React, { useState, useEffect } from "react";
 
 export const Skills = () => {
+  const [counts, setCount] = useState(1);
+  const [count2, setCount2] = useState(1);
+  const [count3, setCount3] = useState(1);
+
+  useEffect(() => {
+    const interval1 = setInterval(() => {
+      if (counts < 100) {
+        setCount(counts + 1);
+      } else {
+        setCount(1); // Reset count to 1
+      }
+    }, 50);
+
+    return () => {
+      clearInterval(interval1);
+    };
+  }, [counts]);
+
+  useEffect(() => {
+    const interval2 = setInterval(() => {
+      if (count2 < 100) {
+        setCount2(count2 + 1);
+      } else {
+        setCount2(1); // Reset count to 1
+      }
+    }, 70);
+
+    return () => {
+      clearInterval(interval2);
+    };
+  }, [count2]);
+
+  useEffect(() => {
+    const interval3 = setInterval(() => {
+      if (count3 < 100) {
+        setCount3(count3 + 1);
+      } else {
+        setCount3(1); // Reset count to 1
+      }
+    }, 80);
+
+    return () => {
+      clearInterval(interval3);
+    };
+  }, [count3]);
+
   return (
     <section className="skillsPage" id="skills">
       <div className="container">
         <div className="skill-bx wow zoomIn">
           <h2>SKILLS</h2>
           <div className="skillColom">
-            <strong>Best First-Person Shooters</strong>
+            <strong>Best First-Person Weapons</strong>
 
             <ol class="alternating-colors">
               <li>
                 <strong>
-                  Fullstack Developer
-                  <div class="loader"></div>
-                </strong>
-                <p>Fight aliens, wear a head crab, learn about gravity</p>
-              </li>
-              <li>
-                <strong>
-                  Computer Science
+                  <div className="line">
+                    Fullstack Developer [<div className="counts">{counts}%</div>{" "}
+                    ]
+                  </div>
+
                   <div class="loader"></div>
                 </strong>
                 <p>
-                  Fight aliens, wear an AI, learn about screen peeking siblings
+                  Who can build both the front end and the back end of a any
+                  platform
                 </p>
               </li>
               <li>
                 <strong>
-                  Generative AI<div class="loader"></div>
+                  <div className="line">
+                    Computer Science [<div className="counts">{count3}%</div> ]
+                  </div>
+                  <div class="loader"></div>
                 </strong>
                 <p>
-                  Fight non-alien residents, wear hats, learn about teamwork
+                  Design and develop applications for various domains, such as
+                  artificial intelligence, web development, data science, and
+                  cybersecurity.
                 </p>
               </li>
               <li>
                 <strong>
-                  Artificial Intelligence (AI)
+                  <div className="line">
+                    Artificial Intelligence (AI) [
+                    <div className="counts">{count2}%</div> ]
+                  </div>
                   <div class="loader"></div>
                 </strong>
-                <p>Ski, slip, and slide your way to victory</p>
+                <p>
+                  It is a field of study in computer science that develops and
+                  studies intelligent machines.
+                </p>
+              </li>
+              <li>
+                <strong>
+                  <div className="line">
+                    Generative AI [ <div className="counts">{counts}%</div> ]
+                  </div>
+                  <div class="loader"></div>
+                </strong>
+                <p>
+                  Artificial intelligence capable of generating text, images, or
+                  other media, using generative models.
+                </p>
               </li>
             </ol>
           </div>
